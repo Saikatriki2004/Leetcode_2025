4836816c275aaecca4cca5b45f1f918e52575e81
Saikat Rana <149765443+Saikatriki2004@users.noreply.github.com>
2025-02-22 18:16:21 +0530

    1028. Recover a Tree From Preorder Traversal.

--- January_2025/Jan_27.py
+++ January_2025/Jan_27.py
@@ -1,3 +1,5 @@
+from typing import List
+
 class Solution:
     def checkIfPrerequisite(self, numCourses: int, prerequisites: List[List[int]], queries: List[List[int]]) -> List[bool]:
         # Step 1: Initialize the reachability matrix
